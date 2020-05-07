29e5d17bfeb79c1c0857b61d930be84d7f363f96
Yann Papouin <y.papouin@dec-industrie.com>
2020-05-07 16:00:37 +0200

    Account invoice aeroo report module added

---
+++ account_report_aeroo/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Account (aeroo report)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Invoice report''',
+    'depends': [
+        'dec_report_aeroo',
+        'company_fax',
+        'account',
+        'account_workflow_dec',
+    ],
+    'data':
+        [
+            'report/account_invoice.xml',
+        ],
+    'installable': True
+}
