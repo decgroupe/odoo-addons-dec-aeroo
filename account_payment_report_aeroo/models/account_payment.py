6d4397ff4c8b5b3fc5e76311af0004fefca636fd
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-17 09:34:22 +0200

    Pylint remove unused import

--- account_payment_report_aeroo/models/account_payment.py
+++ account_payment_report_aeroo/models/account_payment.py
@@ -2,7 +2,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, May 2020
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class AccountPayment(models.Model):
--- account_report_aeroo/models/account_invoice_line.py
+++ account_report_aeroo/models/account_invoice_line.py
@@ -2,7 +2,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Mar 2020
 
-from odoo import api, fields, models
+from odoo import models
 
 class AccountInvoiceLine(models.Model):
     _inherit = ['account.invoice.line', 'report.line.mixin']
