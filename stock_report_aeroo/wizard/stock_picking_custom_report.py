3a886f09b5a6b299d7a3e8625d3ee9f0b6c5447d
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-25 16:10:00 +0100

    Picking report stock move states to print can now be selected

--- stock_report_aeroo/wizard/stock_picking_custom_report.py
+++ stock_report_aeroo/wizard/stock_picking_custom_report.py
@@ -14,6 +14,33 @@ class StockPickingCustomReport(models.TransientModel):
         help="Allow to display a custom text",
     )
 
+    state_draft = fields.Boolean(
+        'New',
+        help='draft',
+    )
+    state_cancel = fields.Boolean(
+        'Cancelled',
+        help='cancel',
+    )
+    state_waiting = fields.Boolean(
+        'Waiting Another Move',
+        help='waiting',
+    )
+    state_confirmed = fields.Boolean(
+        'Waiting Availability',
+        help='confirmed',
+    )
+    state_assigned = fields.Boolean(
+        'Available',
+        default=True,
+        help='assigned or partially_available',
+    )
+    state_done = fields.Boolean(
+        'Done',
+        default=True,
+        help='done',
+    )
+
     @api.multi
     def print_report(self):
         wizard_data = self.read()[0]
@@ -23,6 +50,22 @@ class StockPickingCustomReport(models.TransientModel):
         stock_picking = self.env['stock.picking'].browse(
             context.get('active_ids')
         )
+
+        states = []
+        if self.state_draft:
+            states.append('draft')
+        if self.state_cancel:
+            states.append('cancel')
+        if self.state_waiting:
+            states.append('waiting')
+        if self.state_confirmed:
+            states.append('confirmed')
+        if self.state_assigned:
+            states.append('partially_available')
+            states.append('assigned')
+        if self.state_done:
+            states.append('done')
+        wizard_data['states'] = states
 
         action = report.with_context(context).report_action(
             stock_picking,
