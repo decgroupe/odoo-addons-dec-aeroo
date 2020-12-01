27779e5116a73f86c21a42e984a372df396674fe
Yann Papouin <y.papouin@dec-industrie.com>
2020-12-01 17:32:39 +0100

    Project task aeroo report added

---
+++ project_report_aeroo/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Project (aeroo report)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Project report''',
+    'depends': [
+        'project',
+        'company_fax',
+        'dec_report_aeroo',
+    ],
+    'data':
+        [
+            'report/project_task_intervention_sheet.xml',
+        ],
+    'installable': True
+}
---
+++ project_report_aeroo/models/product_template.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Dec 2020
+
+from odoo import models, api
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    @api.multi
+    def _get_aeroo_report_filename(self):
+        names = [x.name or str(x.id) for x in self]
+        res = '-'.join(names)
+        return res
