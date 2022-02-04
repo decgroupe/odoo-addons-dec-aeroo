e7ee33b804728b79234f4a053b14f629961f6b6d
Yann Papouin <ypa@decgroupe.com>
2022-02-04 16:28:25 +0100

    Use product variant identifier for QRCode generation

--- product_report_aeroo/models/product_product.py
+++ product_report_aeroo/models/product_product.py
@@ -2,6 +2,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Jan 2022
 
+import json
 from odoo import models
 
 
@@ -10,7 +11,16 @@ class ProductProduct(models.Model):
 
     def get_qrcode_data(self):
         self.ensure_one()
-        return self.product_tmpl_id.get_qrcode_data()
+        # In contrary to the `product.template` implementation, we set the
+        # `vid` property instead of `id` to differentiate labels
+        res = {
+            "vid": self.id,
+            "code": self.default_code,
+            "categ": {
+                "id": self.categ_id.id,
+            }
+        }
+        return json.dumps(res)
 
     def get_manufacturer(self):
         self.ensure_one()
